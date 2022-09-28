1138a1b298975f0e70d28638dd09e5fdf2dee24b
Samuel Szajbely <s.szajbely@gmail.com>
2022-09-28 13:47:18 +0000

    fix

--- app/Policies/CommunityServicePolicy.php
+++ app/Policies/CommunityServicePolicy.php
@@ -12,21 +12,43 @@ class CommunityServicePolicy
 {
     use HandlesAuthorization;
 
+
+    /**
+     * Determine whether the user can view any community services.
+     * @param User $user
+     * @return bool
+     */
     public function view(User $user)
     {
         return $user->isCollegist();
     }
 
+    /**
+     * Determine whether the user can create new community service.
+     * @param User $user
+     * @return bool
+     */
     public function create(User $user)
     {
         return $user->isCollegist() && $user->isActive();
     }
 
+    /**
+     * Determine whether the user can be an approver for any community service.
+     * @param User $user
+     * @return bool
+     */
     public function approveAny(User $user)
     {
         return $user->hasRole([Role::STUDENT_COUNCIL]);
     }
 
+    /**
+     * Determine whether the user is the approver for the given community service.
+     * @param User $user
+     * @param CommunityService $communityService
+     * @return bool
+     */
     public function approve(User $user, CommunityService $communityService)
     {
         return $communityService->approver->id === $user->id;
