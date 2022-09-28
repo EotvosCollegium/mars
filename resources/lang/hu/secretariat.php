f9ffcda51a4828ee45c26743748267b32159d229
Sámuel Szajbély <44438989+machiato32@users.noreply.github.com>
2022-09-28 22:36:14 +0200

    Status form improvements & fix (#126)
    * fix
    
    * Format code with php-cs-fixer
    
    * fix
    
    * Format code with php-cs-fixer
    
    * fix
    
    * fix
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- resources/lang/hu/secretariat.php
+++ resources/lang/hu/secretariat.php
@@ -6,6 +6,8 @@ return [
     'semester_status' => 'Szemeszter státusz',
     'semester_status_info' => 'Aktív - Aktív tagja leszel a Collegiumnak<br>Passzív - Külföldi féléven leszel vagy passzív vagy az egyetemen<br>Deaktivált - Nem leszel aktív tagja a Collégiumnak',
     'status' => 'Státusz',
+    'status_set' => 'A félévre a státuszod már be lett állítva:',
+    'status_statement_not_available' => 'A státuszodat már nem tudod frissíteni a félévben. Már megadtad a státuszodat és lejárt a határidő is.',
     'status_statement_request' => 'Add meg a státuszodat a következő szemeszterre! Amennyiben nem töltöd ki :deadline-ig, elveszíted a státuszod!',
     'status_statement_request_subject' => 'Add meg a státuszod a következő félévre!',
     'status_update' => 'Státusz frissítése',
