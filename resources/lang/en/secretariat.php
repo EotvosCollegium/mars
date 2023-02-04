94cc218e5d223effce35f8f5b88404eca461c330
Katkó Dominik <56202545+kdmnk@users.noreply.github.com>
2023-02-04 16:02:22 +0100

    statement-form-fixes (#205)

--- resources/lang/en/secretariat.php
+++ resources/lang/en/secretariat.php
@@ -7,6 +7,7 @@ return [
     'semester_status_info' => 'Active - You will be an active member of the Collegium<br>Passive - You will be on a foregin semester or are passive in the university<br>Deactivated - You will not be an active member of the Collegium',
     'status' => 'Status',
     'status_set' => 'Your status for this semester has already been set, it is',
+    'status_statement' => 'Status statement',
     'status_statement_not_available' => 'The status update form is not available. You have already set your status and the deadline to set it has passed.',
     'status_statement_request' => 'Please update your status for the coming semester. If you fail to fill out the form until the 15th of september, your will lose your status!',
     'status_statement_request_subject' => 'Status statement request',
