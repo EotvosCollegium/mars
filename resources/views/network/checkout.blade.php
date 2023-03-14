e76ba0ff852f0f0f765ba050747d8713f4ae6ded
Katkó Dominik <56202545+kdmnk@users.noreply.github.com>
2023-03-14 22:39:36 +0100

    Add checkout handler info and generalised checkout status card (#223)
    * checkout page descriptions
    
    * checkout handler info and generalised status card

--- resources/views/network/checkout.blade.php
+++ resources/views/network/checkout.blade.php
@@ -10,33 +10,7 @@
 
 <div class="row">
     <div class="col s12">
-        <div class="card">
-            <div class="card-content">
-                <span class="card-title">Kassza</span>
-                <blockquote>
-                    Jelenlegi összeg:
-                    <b class="coli-text text-orange"> {{ number_format($current_balance, 0, '.', ' ') }} Ft</b>.<br>
-                </blockquote>
-                @can('administrate', $checkout)
-                <blockquote>
-                    Jelenlegi összeg a kasszában:
-                    <b class="coli-text text-orange"> {{ number_format($current_balance_in_checkout, 0, '.', ' ') }} Ft</b>.<br>
-                    @if($transactions_not_in_checkout != 0)
-                    Tedd be (ha pozitív) / vedd ki (ha negatív) ezt az összeget a kasszából: <b>{{$transactions_not_in_checkout}} Ft</b>, majd kattints a zöld gombra!
-                    <br>
-                    Figyelem: ebben az összegben a még általad (zsebből) ki nem fizetett vásárlások is benne vannak,
-                    így ha kiveszed az összeget, attól a rendszerben lévő tartozásokat még ki kell elégítened!
-                    @endif
-                </blockquote>
-                @if($transactions_not_in_checkout != 0)
-                    <form method="POST" action="{{ route($route_base . '.to_checkout') }}">
-                        @csrf
-                        <x-input.button floating class="right green" icon="payments"/>
-                    </form>
-                @endif
-                @endcan
-            </div>
-        </div>
+        @include('utils.checkout.status')
         <div class="row">
             <div class="col s12">
                 @include('utils.checkout.add-transaction')
