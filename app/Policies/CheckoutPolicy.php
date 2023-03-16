8fd9d932f0c9ed35184c89b9b07fb5654eba403a
katkodominik <katkodominik@gmail.com>
2023-03-16 21:22:51 +0100

    fix hasRole call in checkout poliicy

--- app/Policies/CheckoutPolicy.php
+++ app/Policies/CheckoutPolicy.php
@@ -43,10 +43,12 @@ class CheckoutPolicy
     public function addKKTNetreg(User $user): bool
     {
         return $user->hasRole([
-            Role::STUDENT_COUNCIL => Role::ECONOMIC_VICE_PRESIDENT,
-            Role::STUDENT_COUNCIL => Role::CULTURAL_LEADER,
-            Role::STUDENT_COUNCIL => Role::CULTURAL_MEMBER,
-            Role::SYS_ADMIN
+            Role::SYS_ADMIN,
+            Role::STUDENT_COUNCIL => [
+                Role::ECONOMIC_VICE_PRESIDENT,
+                Role::CULTURAL_LEADER,
+                Role::CULTURAL_MEMBER
+            ]
         ]);
     }
 
