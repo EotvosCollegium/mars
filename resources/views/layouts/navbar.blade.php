f60e61249b50a8fddba206c43fe6e419c267bbef
viktorcsimma <csimmaviktor03@gmail.com>
2023-06-07 22:51:57 +0200

    Quick fix

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -108,7 +108,7 @@
                     </div>
                 </li>
                 @endcan
-                @if(Gate::allows('is-collegist', $user) || Gate::allows('is-admin', $user))
+                @if(Gate::allows('is-collegist', Auth::user()) || Gate::allows('is-admin', Auth::user()))
                 {{-- Sysadmin module --}}
                 <li class="@yield('admin_module')">
                     <a class="collapsible-header waves-effect" style="padding-left:32px">
