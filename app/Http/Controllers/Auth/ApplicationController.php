7f0b05c2cbcb37e26967d832a3310133c89e0c64
Katkó Dominik <56202545+kdmnk@users.noreply.github.com>
2023-07-27 20:45:12 +0200

    Update app/Http/Controllers/Auth/ApplicationController.php

--- app/Http/Controllers/Auth/ApplicationController.php
+++ app/Http/Controllers/Auth/ApplicationController.php
@@ -50,7 +50,7 @@ class ApplicationController extends Controller
             'faculties' => Faculty::all(),
             'deadline' => self::getApplicationDeadline(),
             'deadline_extended' => self::isDeadlineExtended(),
-            'user' => Auth::user(),
+            'user' => user(),
         ];
         switch ($request->input('page')) {
             case (self::EDUCATIONAL_ROUTE):
