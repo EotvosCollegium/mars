7c3c057c7ce7190c6bf124291c4280f28ab5c90c
Bálint Horcsin <horcsinbalint@gmail.com>
2023-11-25 17:11:33 +0100

    Added debug message

--- app/Utils/Printer.php
+++ app/Utils/Printer.php
@@ -115,12 +115,12 @@ class Printer
         try {
             $command = "lp -d " . $printer_name
                     . ($this->is_two_sided ? " -o sides=two-sided-long-edge " : " ")
-                    . "-n " . $this->number_of_copies . " "
-                    . $this->path . " " . env('PRINTER_ADDITIONAL_ARGS') . " 2>&1";
+                    . "-n " . $this->number_of_copies . " " . env('PRINTER_ADDITIONAL_ARGS') . " "
+                    . $this->path . " 2>&1";
             $result = Commands::print($command);
             if (!preg_match("/^request id is ([^\s]*) \\([0-9]* file\\(s\\)\\)$/", $result, $job)) {
                 Log::error("Printing error at line: " . __FILE__ . ":" . __LINE__ . " (in function " . __FUNCTION__ . "). result:"
-                    . print_r($result, true));
+                    . print_r($result, true) . ". Command: " . $command);
                 $state = PrintJob::ERROR;
             }
             $job_id = $job[1];
