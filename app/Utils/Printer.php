b0c5510179eb8341e4eddc3fc9fe62c14c134077
Bálint Horcsin <horcsinbalint@gmail.com>
2023-11-25 17:55:52 +0100

    Fixed order

--- app/Utils/Printer.php
+++ app/Utils/Printer.php
@@ -113,9 +113,9 @@ class Printer
         $printer_name = config('print.printer_name');
         $state = PrintJob::QUEUED;
         try {
-            $command = "lp -d " . $printer_name
+            $command = "lp " . env('PRINTER_ADDITIONAL_ARGS') . " -d " . $printer_name
                     . ($this->is_two_sided ? " -o sides=two-sided-long-edge " : " ")
-                    . "-n " . $this->number_of_copies . " " . env('PRINTER_ADDITIONAL_ARGS') . " "
+                    . "-n " . $this->number_of_copies . " "
                     . $this->path . " 2>&1";
             $result = Commands::print($command);
             if (!preg_match("/^request id is ([^\s]*) \\([0-9]* file\\(s\\)\\)$/", $result, $job)) {
