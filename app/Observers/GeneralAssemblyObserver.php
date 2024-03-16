7047cbe87ac46a6fc7551bfdd5197720c234240c
Trigary <trigary0@gmail.com>
2024-03-16 23:27:24 +0100

    test general assembly passives get excused

--- app/Observers/GeneralAssemblyObserver.php
+++ app/Observers/GeneralAssemblyObserver.php
@@ -18,8 +18,7 @@ class GeneralAssemblyObserver
     {
         // Excuse passive students. They are excused at the creation of the general assembly (as opposed to when it
         // is opened) because the list of excused students is displayed as soon as the general assembly is created.
-        foreach (Semester::current()->usersWithStatus(SemesterStatus::PASSIVE)->get() as $user) {
-            $generalAssembly->excusedUsers()->attach($user);
-        }
+        $passiveUsers = Semester::current()->usersWithStatus(SemesterStatus::PASSIVE)->get();
+        $generalAssembly->excusedUsers()->attach($passiveUsers);
     }
 }
