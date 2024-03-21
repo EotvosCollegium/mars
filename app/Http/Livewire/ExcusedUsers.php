f22f4f0da87eb911b4596ee9d27a4099700513b6
Gergely Sarkozi <trigary0@gmail.com>
2024-03-21 20:18:42 +0100

    General assembly: excuse passives (fixes #417) (#471)
    * General assembly: excuse passives (fixes #417)
    
    * use observer instead of event
    
    * test general assembly passives get excused
    
    * rename table to general_assembly_excused_users
    
    * add comment to general assembly excused users
    
    * allow manual excuse comment input
    
    * mitigate issue caught by CI
    
    ---------
    
    Co-authored-by: horcsinbalint <51171286+horcsinbalint@users.noreply.github.com>

--- app/Http/Livewire/ExcusedUsers.php
+++ app/Http/Livewire/ExcusedUsers.php
@@ -10,14 +10,17 @@ class ExcusedUsers extends Component
 {
     public GeneralAssembly $general_assembly;
     public $user;
+    public $comment;
 
     public function addUser()
     {
         $this->validate([
             'user' => 'required|exists:users,id',
+            'comment' => 'nullable|string|max:255',
         ]);
-        $this->general_assembly->excusedUsers()->attach($this->user);
+        $this->general_assembly->excusedUsers()->attach($this->user, ['comment' => $this->comment]);
         $this->user = null;
+        $this->comment = null;
     }
 
     public function removeUser($userId)
