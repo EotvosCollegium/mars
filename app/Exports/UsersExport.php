552dcc99f5f4a9f2f01b1924e949b3e929c392c7
Trigary <trigary0@gmail.com>
2024-03-23 23:06:06 +0100

    User list export: apply filters (fixes #298)

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -7,20 +7,32 @@ use App\Exports\UsersSheets\SemesterEvaluationExport;
 use App\Exports\UsersSheets\StatusesExport;
 use App\Exports\UsersSheets\StudentsCouncilFeedback;
 use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 use Maatwebsite\Excel\Concerns\WithDefaultStyles;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 use PhpOffice\PhpSpreadsheet\Style\Style;
 
 class UsersExport implements WithMultipleSheets, WithDefaultStyles
 {
+    private Builder|User $includedUsersQuery;
+
+    public function __construct(Builder|User $includedUsersQuery)
+    {
+        $this->includedUsersQuery = $includedUsersQuery;
+    }
+
     public function sheets(): array
     {
-        $sheets = [new CollegistsExport(), new StatusesExport()];
+        $sheets = [
+            new CollegistsExport($this->includedUsersQuery->clone()),
+            new StatusesExport($this->includedUsersQuery->clone()),
+        ];
 
         if(user()->can('viewSemesterEvaluation', User::class)) {
-            $sheets[] = new SemesterEvaluationExport();
+            $sheets[] = new SemesterEvaluationExport($this->includedUsersQuery->clone());
             if(user()->hasRole(Role::STUDENT_COUNCIL)) {
-                $sheets[] = new StudentsCouncilFeedback(true);
+                $sheets[] = new StudentsCouncilFeedback($this->includedUsersQuery->clone());
             }
         }
 
