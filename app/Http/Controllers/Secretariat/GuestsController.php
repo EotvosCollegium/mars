f10fd80d131a64ff1494e80906b3ff2bd4d0160d
Dominik Katko <katkodominik@gmail.com>
2024-03-31 19:21:37 +0200

    added missing docs

--- app/Http/Controllers/Secretariat/GuestsController.php
+++ app/Http/Controllers/Secretariat/GuestsController.php
@@ -3,20 +3,28 @@
 namespace App\Http\Controllers\Secretariat;
 
 use App\Http\Controllers\Controller;
+use App\Mail\ApprovedRegistration;
 use App\Models\Faculty;
 use App\Models\Role;
 use App\Models\User;
 use App\Models\Workshop;
 use Carbon\Carbon;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\View\View;
 
 class GuestsController extends Controller
 {
+    /**
+     * Display the list of users waiting for approval.
+     * @return View
+     * @throws AuthorizationException
+     */
     public function index()
     {
         $this->authorize('handleGuests', User::class);
@@ -31,6 +39,12 @@ class GuestsController extends Controller
         return view('secretariat.registrations.list', ['users' => $users]);
     }
 
+    /**
+     * Verify a new user.
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws AuthorizationException
+     */
     public function accept(Request $request)
     {
         $this->authorize('handleGuests', User::class);
@@ -49,12 +63,17 @@ class GuestsController extends Controller
 
         Cache::decrement('user');
 
-        // Send notification mail.
-        Mail::to($user)->queue(new \App\Mail\ApprovedRegistration($user->name));
+        Mail::to($user)->queue(new ApprovedRegistration($user->name));
 
         return redirect()->route('secretariat.registrations')->with('message', __('general.successful_modification'));
     }
 
+    /**
+     * Reject and delete a new user.
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws AuthorizationException
+     */
     public function reject(Request $request)
     {
         $this->authorize('handleGuests', User::class);
