8143491d8375771e3224e09151f94b70ac99077d
david <fadedave77@fadedave.com>
2024-04-22 16:12:11 +0000

    Implement KKT role (#512)

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -57,6 +57,7 @@ class Role extends Model
     public const CULTURAL_LEADER = 'cultural-leader';
     public const CULTURAL_REFERENT = 'cultural-referent';
     public const CULTURAL_MEMBER = 'cultural-member';
+    public const KKT_HANDLER = 'kkt-handler';
     public const COMMUNITY_LEADER = 'community-leader';
     public const COMMUNITY_REFERENT = 'community-referent';
     public const COMMUNITY_MEMBER = 'community-member';
@@ -88,6 +89,7 @@ class Role extends Model
         self::COMMUNITY_MEMBER,
         self::COMMUNICATION_MEMBER,
         self::SPORT_MEMBER,
+        self::KKT_HANDLER,
     ];
 
     public const STUDENT_POSTION_ROLES = [
@@ -96,7 +98,7 @@ class Role extends Model
         self::STUDENT_COUNCIL_SECRETARY,
         self::STUDENT_COUNCIL,
         self::BOARD_OF_TRUSTEES_MEMBER,
-        self::ETHICS_COMMISSIONER
+        self::ETHICS_COMMISSIONER,
     ];
 
     // Module-related roles
@@ -124,7 +126,8 @@ class Role extends Model
         self::STUDENT_COUNCIL_SECRETARY,
         self::BOARD_OF_TRUSTEES_MEMBER,
         self::ETHICS_COMMISSIONER,
-        self::ALUMNI
+        self::ALUMNI,
+
     ];
 
     protected $fillable = [
---
+++ database/migrations/2024_04_15_171918_add_kkt_role.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Role;
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('role_objects')->updateOrInsert(['role_id' => Role::firstWhere('name', 'student-council')->id, 'name' => 'kkt-handler'], []);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('role_objects')->where('name', 'kkt-handler')->delete();
+    }
+};
