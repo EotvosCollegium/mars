9538a9470596f67156cb228b6833c741ff68e50a
dave <fadedave77@fadedave.com>
2024-05-03 00:37:29 +0200

    Improve the querypage for KKT/Netreg data, with workshop filtering.

---
+++ app/Livewire/KktNetreg.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Semester;
+use App\Models\User;
+use App\Models\Transaction;
+use App\Models\PaymentType;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\View\View;
+use Livewire\Component;
+
+class KktNetreg extends Component
+{
+    public $workshops = [];
+
+    /**
+     * Return the `users` property.
+     */
+    public function getUnpaidUsersProperty()
+    {
+        $query = User::hasToPayKKTNetreg()->canView();
+
+        $query->where(function (Builder $query) {
+            foreach ($this->workshops as $workshop) {
+                $query->whereHas('workshops', function (Builder $query) use ($workshop) {
+                    $query->where('id', $workshop);
+                });
+            }
+        });
+
+        return $query
+            ->with(['workshops'])
+            ->orderBy('name')->get();
+    }
+
+
+    public function getPaymentsProperty()
+    {
+        $query = Transaction::whereIn('payment_type_id', [PaymentType::kkt()->id, PaymentType::netreg()->id])->where('semester_id', Semester::current()->id);
+
+        $query->where(function (Builder $query) {
+            foreach ($this->workshops as $workshop) {
+                $query->whereHas('payer', function (Builder $query) use ($workshop) {
+                    $query->whereHas('workshops', function (Builder $query) use ($workshop) {
+                        $query->where('id', $workshop);
+                    });
+                });
+            }
+        });
+        // $query->where('payer', function (Builder $query) {
+            //     $query->orderBy('name');
+            // });
+
+        return $query
+            ->with(['payer'])
+            ->get();
+
+
+    }
+
+
+    /**
+     * Add a workshop to filter on.
+     *
+     * @param int $workshop_id
+     */
+    public function addWorkshop($workshop_id)
+    {
+        $this->workshops[] = $workshop_id;
+    }
+
+    /**
+     * Delete a workshop from the list of workshops to filter on.
+     *
+     * @param int $workshop_id
+     */
+    public function deleteWorkshop($workshop_id)
+    {
+        $this->workshops = \array_diff($this->workshops, [$workshop_id]);
+    }
+
+    /**
+     * Render the component.
+     *
+     * @return View
+     */
+    public function render()
+    {
+        return view('student-council.economic-committee.kktnetreg_component');
+    }
+}
