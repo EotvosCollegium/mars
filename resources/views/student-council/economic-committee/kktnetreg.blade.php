39e019953953f0db5bb8e55bc67699a542116c66
dave <fadedave77@fadedave.com>
2024-05-03 22:24:49 +0200

    Fix localization

--- resources/views/student-council/economic-committee/kktnetreg.blade.php
+++ resources/views/student-council/economic-committee/kktnetreg.blade.php
@@ -2,7 +2,7 @@
 
 @section('title')
 <a href="#!" class="breadcrumb">@lang('general.student_council')</a>
-<a href="{{ route('economic_committee') }}" class="breadcrumb" style="cursor: pointer">@lang('general.council_wallet')</a>
+<a href="{{ route('economic_committee') }}" class="breadcrumb" style="cursor: pointer">@lang('general.checkout')</a>
 <a href="#!" class="breadcrumb">@lang('general.kkt_netreg')</a>
 @endsection
 
