8900de200b9c92350eb58df819ef57446427bc3a
Daniel Bertalan <dani@danielbertalan.dev>
2024-07-31 09:11:18 +0200

    Remove large, commented out script
    Even though the code never gets executed as it's wrapped in JavaScript
    comments, the blade templating engine still processes it and emits it in
    the output. In some pages related to the general assembly, this causes
    the names of all users and their user IDs to be included in the page
    source as comments.
    
    We could use blade comments (`{{-- [...] --}}`) if we wanted to keep
    this code around but not include it in the output; but it serves no
    purpose and can be restored from git history if it ends up being needed.
    
    Fixes #514

--- resources/views/components/input/select.blade.php
+++ resources/views/components/input/select.blade.php
@@ -44,20 +44,3 @@
 @if(!$onlyInput)
 </div>
 @endif
-
-@push('scripts')
-    <script>
-        // //Initialize materialize select
-        // var instances;
-        // $(document).ready(
-        // function() {
-        //     var elems = $('#{{ $id }}');
-        //     const options = [
-        //     @foreach ($elements as $element)
-        //         { name : '{{ $element->name ?? $element }}',  value : '{{ $element->id ?? $element }}'},
-        //     @endforeach
-        //     ];
-        //     instances = M.FormSelect.init(elems, options);
-        // });
-</script>
-@endpush
