7d93901ca25222553bfd4c0758279f66664aa4f3
Daniel Bertalan <dani@danielbertalan.dev>
2024-08-09 14:04:44 +0200

    Fix permission check for post-finalization file upload
    The argument to `editStatus` is a `?Workshop`, not an `Application`
    
    If we do not pass it, only "superusers" (secretary, director and student
    council leaders) can edit it. That's the behavior we want.

--- app/Http/Controllers/Auth/AdmissionController.php
+++ app/Http/Controllers/Auth/AdmissionController.php
@@ -166,7 +166,7 @@ class AdmissionController extends Controller
             Mail::bcc($application->committeeMembers())->queue(new ApplicationNoteChanged(user(), $application, $oldValue));
         }
         if ($request->has('file')) {
-            $this->authorize('editStatus', $application);
+            $this->authorize('editStatus');
             $this->storeFile($request, $application->user);
             Mail::bcc($application->committeeMembers())->queue(new ApplicationFileUploaded($request->get('name'), $application));
         }
