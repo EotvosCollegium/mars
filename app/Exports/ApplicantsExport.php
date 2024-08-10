cce6feae441b19f34f91744c3193294fa0c3004b
Daniel Bertalan <dani@danielbertalan.dev>
2024-08-10 02:26:20 +0200

    Use applied workshop list for applicant export (#622)
    `$user->workshops` is only set for users that have already been admitted
    to the Collegium. Use the workshop list from application data for the
    Excel applicant export instead.

--- app/Exports/ApplicantsExport.php
+++ app/Exports/ApplicantsExport.php
@@ -81,7 +81,7 @@ class ApplicantsExport implements FromCollection, WithTitle, WithMapping, WithHe
                 return $studyLine->getNameWithYear();
             })->implode(', '),
             implode(",", $user->faculties->pluck('name')->toArray()),
-            implode(",", $user->workshops->pluck('name')->toArray()),
+            $application->appliedWorkshops()->implode('name', ','),
             $user->isResident() ? 'Bentlakó' : 'Bejáró',
             ($user->educationalInformation?->alfonso_language ?
                 __('role.'.$user->educationalInformation->alfonso_language) . " " . $user->educationalInformation->alfonso_desired_level
