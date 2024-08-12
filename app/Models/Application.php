53d713a803b6148d66f62ec5924924153593f24b
kdmnk <katkodominik@gmail.com>
2024-08-12 10:55:39 +0200

    fix wrong import

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -12,7 +12,6 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Collection;
-use Matrix\Builder;
 
 /**
  * App\Models\Application
