271081b448da08de9b4fa9c8ed8d017ae7859674
Viktor Csimma <csimmaviktor03@gmail.com>
2024-10-01 20:29:09 +0200

    Adding a modal dialog for providing a message

--- resources/views/dormitory/reservations/item_table.blade.php
+++ resources/views/dormitory/reservations/item_table.blade.php
@@ -28,18 +28,49 @@
                 </a>
             </td>
             <td>
+                {{-- The button triggers a modal dialog in which the user can provide an explanation about the problem. --}}
+
+                {{-- modal trigger --}}
+                <a @class([
+                    'btn',
+                    'waves-effect',
+                    'waves-light',
+                    'modal-trigger',
+                    'right',
+                    'coli' => !$item->out_of_order,
+                    'blue' => !$item->out_of_order,
+                ]) class="" href="#fault-reporting-modal-{{ $item->id }}">{{ __($item->out_of_order ? 'reservations.report_fix' : 'reservations.report_fault') }}</a>
+
+                {{-- modal structure --}}
                 <form method="POST"
                 action="{{ route('reservations.items.report_fault', ['item' => $item]) }}"
                 enctype='multipart/form-data'>
                     @csrf
-                    <x-input.button @class([
-                        'right',
-                        'coli' => !$item->out_of_order,
-                        'blue' => !$item->out_of_order,
-                    ]) :text="__($item->out_of_order ? 'reservations.report_fix' : 'reservations.report_fault')" />
+                    <div id="fault-reporting-modal-{{ $item->id }}" class="modal">
+                        <div class="modal-content">
+                            <h4>{{ __($item->out_of_order ? 'reservations.report_fix' : 'reservations.report_fault') }}</h4>
+                            <x-input.textarea id="message"
+                                      :text="__('general.description')"
+                                      :helper="__('reservations.describe_what_happened')"/>
+                        </div>
+                        <div class="modal-footer">
+                            <a href="#!" type="submit" class="waves-effect btn modal-close">@lang('general.cancel')</a>
+                            <button class="waves-effect btn modal-close" type="submit">@lang('general.send')</button>
+                        </div>
+                    </div>
                 </form>
+
             </td>
         </tr>
         @endforeach
     </tbody>
 </table>
+
+@push('scripts')
+<script>
+// for the modal dialogs
+$(document).ready(function(){
+    $('.modal').modal();
+});
+</script>
+@endpush
