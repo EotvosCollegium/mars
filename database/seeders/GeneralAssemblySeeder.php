c8067e91f1e75d46bfff07b3053ee14144f9a80b
Viktor Csimma <csimmaviktor03@gmail.com>
2025-02-13 14:42:10 +0100

    Adding an STV question with random ballots to the seeder

--- database/seeders/GeneralAssemblySeeder.php
+++ database/seeders/GeneralAssemblySeeder.php
@@ -39,7 +39,7 @@ class GeneralAssemblySeeder extends Seeder
         ]);
 
         $openCheckboxQuestion = $openSitting->questions()->create([
-            'title' => "Curatorium members",
+            'title' => "Curatorium members (multiple choice)",
             'max_options' => 3,
             'question_type' => Question::SELECTION,
             'opened_at' => now()
@@ -66,6 +66,54 @@ class GeneralAssemblySeeder extends Seeder
         foreach(User::collegists() as $collegist) {
             $openQuestion->storeAnswers($collegist, [$openQuestion->options->random()]);
             $openCheckboxQuestion->storeAnswers($collegist, $openCheckboxQuestion->options->random(2)->all());
+        }
+
+        $openRankingQuestion = $openSitting->questions()->create([
+            'title' => "Curatorium members (STV)",
+            'max_options' => 5,
+            'question_type' => Question::RANKING,
+            'opened_at' => now()
+        ]);
+        $openRankingQuestion->options()->create([
+            'title' => "Űrlaki Anna Őzike",
+        ]);
+        $openRankingQuestion->options()->create([
+            'title' => "Peter Venkman",
+        ]);
+        $openRankingQuestion->options()->create([
+            'title' => "Ray Stantz",
+        ]);
+        $openRankingQuestion->options()->create([
+            'title' => "Egon Spengler",
+        ]);
+        $openRankingQuestion->options()->create([
+            'title' => "Winston Zeddemore",
+        ]);
+        $openRankingQuestion->options()->create([
+            'title' => "Dana Barrett",
+        ]);
+        $openRankingQuestion->options()->create([
+            'title' => "Louis Tully",
+        ]);
+        $openRankingQuestion->options()->create([
+            'title' => "Janine Melnitz",
+        ]);
+        $openRankingQuestion->options()->create([
+            'title' => "Walter Peck",
+        ]);
+        $rankingOptionIds = $openRankingQuestion->options()->pluck('id')->all();
+
+        // generating random ranking votes
+        $rankingVoteCount = rand(0, 300);
+        for ($i = 0; $i < $rankingVoteCount; ++$i) {
+            shuffle($rankingOptionIds);
+            $orderedOptionCount = rand(0, count($rankingOptionIds)-1);
+            $answer = array_slice($rankingOptionIds, 0, $orderedOptionCount);
+            // we won't connect them to users,
+            // just write them among the answers
+            $openRankingQuestion->longAnswers()->create([
+                'text' => json_encode($answer)
+            ]);
         }
     }
 }
