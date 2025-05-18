7918ef49a4736f24c52ef83f7d5130567bb82282
Balint Horcsin <horcsinbalint@gmail.com>
2025-05-18 18:20:59 +0200

    Remove string processing done via shell and #486
    Using shell scripts to transform text contributes quite a bit to the
    smell of the codebase. We also had issues because of this (`grep`
    arbitrarily decided sometimes for outputs of `pdfinfo` that they were
    binary data and not text). While removing calls to
    `Process::fromShellCommandline` (which is quite unsafe), I also moved
    from `ping` to `nmap` (fixing #486).

--- config/commands.php
+++ config/commands.php
@@ -3,8 +3,8 @@
 return [
     'lpstat' => env('LPSTAT_COMMAND', 'lpstat'),
     'cancel' => env('CANCEL_COMMAND', 'cancel'),
+    'nmap' => env('NMAP_COMMAND', 'nmap'),
     'pdfinfo' => env('PDFINFO_COMMAND', 'pdfinfo'),
-    'ping' => env('PING_COMMAND', 'ping'),
     'pdflatex' => env('PDFLATEX_COMMAND', '/usr/bin/pdflatex'),
     'run_in_debug' => env('RUN_COMMANDS_IN_DEBUG_MODE', false),
 ];
