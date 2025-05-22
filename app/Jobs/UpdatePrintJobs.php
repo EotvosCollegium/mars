224e0109af043ca85b761725dfd3bf0eec47611a
Balint Horcsin <horcsinbalint@gmail.com>
2025-05-22 16:45:44 +0200

    Do not call `lpstat` on all printing page loading
    Calling `lpstat` every time the printing page is loaded is extremely
    time consuming and not even that useful. Moving it to its own seperate
    job that runs every 30 seconds.
    
    Fixes #375

---
+++ app/Jobs/UpdatePrintJobs.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Console\Commands;
+use App\Models\PrintJob;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class UpdatePrintJobs implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        PrintJob::updateCompletedPrintJobs();
+    }
+}
