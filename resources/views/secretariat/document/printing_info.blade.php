b029cd173c825fc83b17be75edb711376ea00bcd
Balint Horcsin <horcsinbalint@gmail.com>
2025-07-09 21:36:46 +0200

    Fixing nits

--- resources/views/secretariat/document/printing_info.blade.php
+++ resources/views/secretariat/document/printing_info.blade.php
@@ -1,9 +1,12 @@
 @if($printing_available)
     <blockquote>
-        {{__("document.printing_first", [
-            "printing_conf_name_hun" => $printing_conf_name_hun,
-            "printing_conf_name_eng" => $printing_conf_name_eng,
-            "current_balance" => $current_balance
-        ])}}<a href="{{route('print.index')}}">{{__("document.printing_second")}}</a>{{__("document.printing_third")}}
+        @markdown(__("document.printing_first",
+            [
+                "printing_conf_name_hun" => $printing_conf_name_hun,
+                "printing_conf_name_eng" => $printing_conf_name_eng,
+                "current_balance" => $current_balance,
+                "printing_page_url" => route('print.index')
+            ]
+        ))
     </blockquote>
 @endif
