a8f4f969e8762e37a3b31c20f801e053be0272fe
Balint Horcsin <horcsinbalint@gmail.com>
2025-07-11 21:26:10 +0000

    Added hideable pictures

--- resources/views/auth/admission/index.blade.php
+++ resources/views/auth/admission/index.blade.php
@@ -80,6 +80,9 @@
         @include('auth.application.application', ['user' => $application->user, 'expanded' => false])
     @endforeach
     <hr>
+    <div class="row center">
+        <button type="button" class="btn grey toggle-hideable-pictures"></button>
+    </div>
     <h6>Összesen: <b class="right">{{$applications->count()}} jelentkező</b></h6>
     @can('finalize', \App\Models\Application::class)
         @if($applicationDeadline && $applicationDeadline < now())
