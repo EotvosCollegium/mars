c5396d3be4f86cf9f7c28d91faba60652620c7a3
Daniel Bertalan <dani@danielbertalan.dev>
2025-07-19 18:29:40 +0200

    Maximize profile picture size

--- resources/views/auth/application/application.blade.php
+++ resources/views/auth/application/application.blade.php
@@ -6,7 +6,7 @@
                 <div class="col s12 xl4">
                     <div style="display:inline-block;width:100%">
                         @if ($user->profilePicture)
-                            <img src="{{ url($user->profilePicture->path) }}" style="max-width:100%"
+                            <img src="{{ url($user->profilePicture->path) }}" style="max-width:100%;max-height:200px;"
                             @if ($user->id != user()->id)
                                 class="hideable-picture"
                             @endif
