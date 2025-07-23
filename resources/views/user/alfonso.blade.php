33601bd48264a3f851ff2a99d40fc9704e438ae4
Horcsin Bálint <horcsinbalint@gmail.com>
2025-07-23 21:12:37 +0000

    Make easier to understand ALFONSO null

--- resources/views/user/alfonso.blade.php
+++ resources/views/user/alfonso.blade.php
@@ -36,14 +36,14 @@ Felvételt követően lehetőség lesz módosítani a nyelven és a szinten.</p>
         <x-input.select l=5 id="alfonso_language" text="Az Alfonsó program keretében választott nyelv"
                     value='{{ $user->educationalInformation?->alfonso_language }}'
                     :elements="App\View\Components\Input\Select::convertArray(config('app.alfonso_languages'))"
-                    allow-empty="{{ isset($application) ? 'A szabályzat alapján nem fogok részt venni az ALFONSÓ programban' : 'Nem tanulok ALFONSÓt' }}"
+                    allow-empty="{{ isset($application) ? '-' : 'Nem tanulok ALFONSÓt' }}"
                     :disabled="user()->cannot('editStaticEducationalInformation', $user)"
                     :asterisk="!$user->educationalInformation->alfonsoExempted()"
                     />
         <x-input.select l=5 id="alfonso_desired_level" text="Elérni kívánt szint"
             :value='$user->educationalInformation?->alfonso_desired_level'
             :elements="['B2','C1']"
-            allow-empty="{{ isset($application) ? 'A szabályzat alapján nem fogok részt venni az ALFONSÓ programban' : 'Nem tanulok ALFONSÓt' }}"
+            allow-empty="{{ isset($application) ? '-' : 'Nem tanulok ALFONSÓt' }}"
             :disabled="user()->cannot('editStaticEducationalInformation', $user)"
             :asterisk="!$user->educationalInformation->alfonsoExempted()"
         />
