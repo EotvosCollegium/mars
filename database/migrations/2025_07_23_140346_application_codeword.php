a4482ddf550abbfbc4518235cd77a59b6e894e6a
Daniel Bertalan <dani@danielbertalan.dev>
2025-07-23 14:52:17 +0200

    Add opt-out for publishing results + codeword field

---
+++ database/migrations/2025_07_23_140346_application_codeword.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table->boolean('publication_consent')->default(false)->nullable(false)->after('accommodation');
+            $table->string('codeword', 20)->nullable()->after('publication_consent');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table->dropColumn('publication_consent');
+            $table->dropColumn('codeword');
+        });
+    }
+};
