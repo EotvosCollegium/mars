584e6ef5b97da749bcb15705948b8d3a7d761bdb
Daniel Bertalan <dani@danielbertalan.dev>
2026-02-15 00:20:20 +0100

    Optimize database access on checkouts some more

--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -67,7 +67,7 @@ class Checkout extends Model
                 'transactions' => function ($query) {
                     $query->where('checkout_id', $this->id);
 
-                    $query->with(['type', 'receipt']);
+                    $query->with(['type', 'receipt', 'payer']);
                 },
                 'workshopBalances.workshop',
             ]);
