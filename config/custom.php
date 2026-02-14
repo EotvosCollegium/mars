a9d21f82ef353f4c31b5edbca6168ee6a11a1ba8
Daniel Bertalan <dani@danielbertalan.dev>
2026-02-14 13:49:39 +0100

    Remove unused configuration variables

--- config/custom.php
+++ config/custom.php
@@ -9,13 +9,6 @@ return [
     'workshop_balance_resident' => env('WORKSHOP_BALANCE_RESIDENT', '0.6'),
     'workshop_balance_extern' => env('WORKSHOP_BALANCE_EXTERN', '0.45'),
 
-    'mr_and_miss_deadline' => env('MR_AND_MISS_DEADLINE'),
-
-    'application_deadline' => env('APPLICATION_DEADLINE'), //registration deadline for new collegists
-    'application_extended' => env('APPLICATION_EXTENDED', false), //is the registration deadline extended?
-
-    'semester_evaluation_deadline' => env('SEMESTER_EVALUATION_DEADLINE'),
-
     // maximum accepted file size in kilobytes (also applies for profile pictures)
     'general_file_size_limit' => env('GENERAL_FILE_SIZE_LIMIT', 10000),
 ];
